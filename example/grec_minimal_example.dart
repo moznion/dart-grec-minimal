c01a44e800f84c1162e9bb2765b366b887aaac13
moznion <moznion@gmail.com>
2018-06-06 22:33:26 +0900

    Initial commit

---
+++ example/grec_minimal_example.dart
@@ -0,0 +1,6 @@
+import 'package:grec_minimal/grec_minimal.dart';
+
+main() {
+  var awesome = new Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/grec_minimal.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library grec_minimal;
+
+export 'src/grec_minimal_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
