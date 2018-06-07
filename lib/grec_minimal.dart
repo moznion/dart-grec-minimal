e2962b644edcfdc4dc75633996f66e2d3de7fcfe
moznion <moznion@gmail.com>
2018-06-07 17:22:06 +0900

    Add entry point of this library

--- lib/grec_minimal.dart
+++ lib/grec_minimal.dart
@@ -3,6 +3,4 @@
 /// More dartdocs go here.
 library grec_minimal;
 
-export 'src/grec_minimal_base.dart';
-
-// TODO: Export any libraries intended for clients of this package.
+export 'src/grec_minimal.dart';
